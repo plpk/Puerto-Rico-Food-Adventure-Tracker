c51e27b008b1245e1ead63a6b481a1403b15e799
thinair <thinair@thinairs-MacBook-Air.local>
2024-11-03 21:35:54 -0400

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Puerto-Rico-Food-Adventure-Tracker/',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
+  base: '/Puerto-Rico-Food-Adventure-Tracker/'
 })
